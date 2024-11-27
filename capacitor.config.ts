4464cbdbf4d243f7dfd6e5539af4c55ea1a160d6
AncorGG <ancorgarcia63@gmail.com>
2024-11-27 21:45:47 +0000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Axon-Ionic',
+  webDir: 'www'
+};
+
+export default config;
