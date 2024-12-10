fc54a4644645ea74db994bc4713248e772449edb
unknown <ancorgarcia63@gmail.com>
2024-12-10 17:44:34 +0000

    Style Update

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,18 +3,19 @@ import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 import { FormsModule } from '@angular/forms';
 import { HomePage } from './home.page';
-import { HttpClientModule } from '@angular/common/http'; 
+import { HttpClientModule } from '@angular/common/http';
 import { HomePageRoutingModule } from './home-routing.module';
-
+import { ComponentsModule } from '../components/components.module';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
+    ComponentsModule,
     HomePageRoutingModule,
-    HttpClientModule
+    HttpClientModule,
   ],
-  declarations: [HomePage]
+  declarations: [HomePage],
 })
 export class HomePageModule {}
--- src/app/routines/routines.module.ts
+++ src/app/routines/routines.module.ts
@@ -1,20 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-
 import { IonicModule } from '@ionic/angular';
-
 import { RoutinesPageRoutingModule } from './routines-routing.module';
-
 import { RoutinesPage } from './routines.page';
+import { ComponentsModule } from '../components/components.module';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    RoutinesPageRoutingModule
+    ComponentsModule,
+    RoutinesPageRoutingModule,
   ],
-  declarations: [RoutinesPage]
+  declarations: [RoutinesPage],
 })
 export class RoutinesPageModule {}
